2d62963521bfe9abae115b0f03c99cf765a194fb
KINOKI <raphael@lesitevideo.net>
2020-12-20 11:24:57 +0100

    client sur raspberry

---
+++ client/client.js
@@ -0,0 +1,32 @@
+// node client.js | aplay -f S16_LE  -c1 -r 44100 -B 100000
+
+const macpro_ip = 'http://192.168.2.22:1664';
+const BLOCK_ID = 23;
+var myid;
+
+var stdout = require('stdout-stream');
+
+var socket = require('socket.io-client')( macpro_ip,{
+  query: {
+    block_id: BLOCK_ID
+  }
+});
+
+socket.binaryType = 'arraybuffer';
+
+socket.on('connect', function(){
+	myid = socket.id;
+	console.log('connected with id : ' , socket.id);
+});
+
+socket.on('binaryData', function( data ){
+	process.stdout.write( data.stream );
+});
+
+socket.on('stop stream', function ( id ){
+
+});
+
+socket.on('disconnect', function(){
+	console.log('disconnected');
+});
