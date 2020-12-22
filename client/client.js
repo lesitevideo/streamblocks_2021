d77813116eef3ad175cac0e0c468218b7ed14be5
KINOKI <raphael@lesitevideo.net>
2020-12-22 10:30:52 +0100

    Add files via upload

--- client/client.js
+++ client/client.js
@@ -1,5 +1,4 @@
 // node client.js | aplay -f S16_LE  -c1 -r 44100 -B 100000
-//node client.js | aplay -f S16_LE -c1 -r 48000 -B 100000  --buffer-size=1024
 
 const macpro_ip = 'http://192.168.2.22:1664';
 const BLOCK_ID = 23;
@@ -21,7 +20,9 @@ socket.on('connect', function(){
 });
 
 socket.on('binaryData', function( data ){
+	
 	process.stdout.write( data.stream );
+	
 });
 
 socket.on('stop stream', function ( id ){
