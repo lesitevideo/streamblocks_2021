1fb540453bc6f12a31d6d0ffe5be60b174535f93
KINOKI <raphael@lesitevideo.net>
2020-12-24 13:48:32 +0100

    Add files via upload

--- client/client.js
+++ client/client.js
@@ -1,15 +1,14 @@
 // node client.js | aplay -f S16_LE  -c1 -r 44100 -B 100000
-//var Sound = require('./aplay.js');
-//var child_process = require('child_process');
 
 const macpro_ip = 'http://192.168.2.22:1664';
 const BLOCK_ID = 23;
-var myid;
+
+var child_process = require('child_process');
 
 const { networkInterfaces } = require('os');
 
 const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
+const results = Object.create(null); // {}
 
 for (const name of Object.keys(nets)) {
     for (const net of nets[name]) {
@@ -23,9 +22,7 @@ for (const name of Object.keys(nets)) {
     }
 }
 
-//console.log(results.wlan0[0]);
-
-var stdout = require('stdout-stream');
+var is_spawned = false;
 
 var socket = require('socket.io-client')( macpro_ip,{
   query: {
@@ -33,7 +30,9 @@ var socket = require('socket.io-client')( macpro_ip,{
   }
 });
 
-//var music = new Sound();
+socket.binaryType = 'arraybuffer';
+
+
 var args = [
 		'-f','S16_LE',
 		'-c1',
@@ -41,34 +40,51 @@ var args = [
 		'-B','5333'
 ];
 
-//const child = child_process.spawnSync( "aplay", args ); //, {stdio: [ process.stdin, process.stdout, process.stderr ]}
+var child;
 
-socket.binaryType = 'arraybuffer';
 
 socket.on('connect', function(){
-	myid = socket.id;
 	console.log('connected with id : ' , socket.id);
-	
-});
-
-socket.on('binaryData', function( data ){
-	
-	
-	//console.log( child.stdout );
-	//child.stdout.write( data.stream );
-	
-	//music.play( data.stream );
-	process.stdout.write( data.stream );
-	
-});
-
-socket.on('stop stream', function ( id ){
-	
-	//process.stdout.write( '' );
-	
 });
 
 socket.on('disconnect', function(){
 	console.log('disconnected');
 });
+
+socket.on('set args', function( args ){
+	console.log('args envoyés : ', args);
+});
+
+socket.on('binaryData', function( data ){
+	
+	if( is_spawned ){
+		child.stdin.write( data.stream );
+	} else {
+		args= [
+			'-f','S16_LE',
+			'-c1',
+			'-r', data.sampleRate,
+			'-B', parseInt( data.latency * 1000000 ) //<- seconds to microsecondes
+		];
+		span_childprocess();
+	}
+	//process.stdout.write( data.stream );
+});
+
+socket.on('stop stream', function ( id ){
+	if( is_spawned ){
+		child.kill('SIGTERM');
+		is_spawned = false;
+	}	
+});
+
+
+function span_childprocess(){
+	child = child_process.spawn( "aplay", args );
+	child.stderr.on('data',function(data) {
+		process.stdout.setEncoding('utf8');
+		process.stdout.write('Debug => '+ String( data));
+	});
+	is_spawned = true;
+}
 
